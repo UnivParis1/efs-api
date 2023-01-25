cecde072129171b19af97fedf9cd9e89e48cc145
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-01-25 06:41:27 +0100

    Send model parameter

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -6,10 +6,10 @@ const searchService = {
     process.env.CELERY_BROKER,
     process.env.CELERY_BACKEND,
   ),
-  fetchExperts: async (sentence: String, precision: Number) => {
+  fetchExperts: async (sentence: String, precision: Number, model: String) => {
     const celery_client = searchService.celery_client();
     const task: Task = celery_client.createTask("tasks.find_experts");
-    const result = task.applyAsync([sentence, precision]);
+    const result = task.applyAsync([sentence, precision, model]);
     const data = await result.get();
     await celery_client.disconnect();
     return data;
