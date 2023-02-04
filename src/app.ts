5a8435cb658a62a85d5c59ee97e59813745e74bb
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-02-04 06:23:25 +0100

    Configure rate limiter

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,14 @@ const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+import {rateLimiter} from './middlewares';
 
 const searchRouter = require('./routes/search');
 
 const app = express();
 
 app.use(cors())
-// view engine setup
+
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
@@ -25,26 +26,27 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../efs-gui', "build")));
 
+app.use('/search', rateLimiter);
 app.use('/search', searchRouter);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../../efs-gui', "build", "index.html"));
+    res.sendFile(path.join(__dirname, '../../efs-gui', "build", "index.html"));
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
